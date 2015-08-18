ba864b3ade22010a0df87673b9d9bb6c29ff3a95
Stu <stuart@blueprintinteractive.com>
2015-08-17 21:58:21 -0400

    add routes to controllers

--- app/candidate/candidate.js
+++ app/candidate/candidate.js
@@ -1,5 +1,15 @@
-angular.module('candidate', [])
+angular.module('candidate', ['ngRoute'])
+    .config(config)
     .controller('CandidateCtrl', CandidateCtrl);
+
+
+config.$inject = ['$routeProvider'];
+function config($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'candidate/candidate.html',
+        controller: 'CandidateCtrl'
+    });
+}
 
 CandidateCtrl.$inject = [];
 function CandidateCtrl() {
