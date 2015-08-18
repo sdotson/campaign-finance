81ff5bc008d716f320f53c8f610662ed63177bad
Stu <stuart@blueprintinteractive.com>
2015-08-18 09:17:26 -0400

    more initial work on countries service

--- app/candidates.js
+++ app/candidates.js
@@ -1,11 +1,42 @@
 angular.module('candidates', [])
     .factory('candidatesService', candidatesService);
 
-candidatesService.$inject = [];
-function candidatesService() {
+candidatesService.$inject = ['$http'];
+function candidatesService($http) {
     var candidatesService = {
-
+        getCandidates: getCandidates
     };
+
+    function getCandidates() {
+        var request = $http({
+            url: '',
+            method: 'GET',
+            cache: true,
+            params: {
+
+            }
+        });
+        return request.then(countriesSuccess, countriesError);
+    }
+
+    function countriesSuccess(response) {
+        console.log(response);
+    }
+
+    function countriesError(response) {
+        console.log(response);
+    }
 
     return candidatesService;
 }
+
+
+/*
+
+http://sunlightlabs.github.io/realtime-docs/candidates
+
+API key: 5fb0ee006d904354961ae1e83e80011b
+
+
+
+*/
