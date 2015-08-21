39be0be98377bf761c66904a34d003ce9dc8a8a4
Stu <stuart@blueprintinteractive.com>
2015-08-21 10:15:39 -0400

    add filtering mechanism to home view

--- app/home/home.js
+++ app/home/home.js
@@ -17,5 +17,21 @@ function config($routeProvider) {
 
 HomeCtrl.$inject = ['$scope','candidates'];
 function HomeCtrl($scope, candidates) {
+    candidates.forEach(function(c){
+       c.cash_on_hand = parseFloat(c.cash_on_hand);
+       c.total_receipts = parseFloat(c.total_receipts);
+       c.total_contributions = parseFloat(c.total_contributions);
+       c.total_disbursements = parseFloat(c.total_disbursements);
+       c.outstanding_loans = parseFloat(c.outstanding_loans);
+    });
+
     $scope.candidates = candidates;
+
+    $scope.parseInt = parseInt;
+
+    /*$scope.criteria = {
+        sortBy: "cash_on_hand",
+        sortDirection: "+"
+    };*/
+
 }
