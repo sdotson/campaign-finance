fd56ec02bdce9532d8261fd89a1ac4b1fc8d33e7
Stu <stuart@blueprintinteractive.com>
2015-08-28 17:48:22 -0400

    add httpInterceptor

--- app/app.js
+++ app/app.js
@@ -1,1 +1,19 @@
-angular.module('campaignFinanceApp', ['cfa.components.home','cfa.components.candidate']);
+angular.module('campaignFinanceApp', ['cfa.components.home','cfa.components.candidate', 'ngResource'])
+
+    .config(config);
+
+
+function config($httpProvider) {
+    $httpProvider.interceptors.push(function ($rootScope) {
+      $rootScope.loading = false;
+      return function (promise) {
+        $rootScope.loading = true;
+        // make sure the loading screen is visible
+        var hide = function (r) {
+            $rootScope.loading = false;
+             return r;
+        };
+        return promise.then(hide, hide);
+      };
+    });
+}
