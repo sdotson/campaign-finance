e32577e301c5a30790f9e12517e69127db29aa77
Stu <stuart@blueprintinteractive.com>
2015-09-11 19:27:59 -0400

    wrap everything in IIFE

--- app/app.js
+++ app/app.js
@@ -1,19 +1,24 @@
-angular.module('campaignFinanceApp', ['cfa.components.home','cfa.components.candidate', 'ngResource', 'ngAnimate'])
+(function(){
+  'use strict';
 
-    .config(config);
+  angular.module('campaignFinanceApp', ['cfa.components.home','cfa.components.candidate', 'ngResource', 'ngAnimate'])
 
-config.$inject = ['$httpProvider'];
-function config($httpProvider) {
-    $httpProvider.interceptors.push(['$rootScope', function($rootScope) {
-      $rootScope.loading = false;
-      return function (promise) {
-        $rootScope.loading = true;
-        // make sure the loading screen is visible
-        var hide = function (r) {
-            $rootScope.loading = false;
-             return r;
+      .config(config);
+
+  config.$inject = ['$httpProvider'];
+  function config($httpProvider) {
+      $httpProvider.interceptors.push(['$rootScope', function($rootScope) {
+        $rootScope.loading = false;
+        return function (promise) {
+          $rootScope.loading = true;
+          // make sure the loading screen is visible
+          var hide = function (r) {
+              $rootScope.loading = false;
+               return r;
+          };
+          return promise.then(hide, hide);
         };
-        return promise.then(hide, hide);
-      };
-    }]);
-}
+      }]);
+  }
+  
+})();
--- app/common/directives/cfaImage.js
+++ app/common/directives/cfaImage.js
@@ -1,19 +1,24 @@
-angular.module('cfa.directives.cfaImage', [])
-    .directive('cfaImage', cfaImage);
+(function() {
+    'use strict';
 
-cfaImage.$inject = [];
-function cfaImage() {
-    return {
-        restrict: "E",
-        link: function(scope, element, attrs) {
-            if (attrs.src) {
-                var img = new Image();
-                img.onload = function() {
-                    element.addClass('active');
-                };
-                element.append(img);            
-                img.src = attrs.src;
-            };  
+    angular.module('cfa.directives.cfaImage', [])
+        .directive('cfaImage', cfaImage);
+
+    cfaImage.$inject = [];
+    function cfaImage() {
+        return {
+            restrict: "E",
+            link: function(scope, element, attrs) {
+                if (attrs.src) {
+                    var img = new Image();
+                    img.onload = function() {
+                        element.addClass('active');
+                    };
+                    element.append(img);            
+                    img.src = attrs.src;
+                };  
+            }
         }
     }
-}
+
+})();
