49a4b091711a25553bd9a06348cc0e9e7252e4dc
Stu <stuart@blueprintinteractive.com>
2015-09-11 19:28:27 -0400

    Update 2015-09-11T23:28:26.260Z

--- components/candidate/candidate.js
+++ components/candidate/candidate.js
@@ -1,51 +1,57 @@
-angular.module('cfa.components.candidate', ['ngRoute','chart.js','cfa.directives.cfaImage'])
-    .config(candidateConfig)
-    .controller('CandidateCtrl', CandidateCtrl);
-
-candidateConfig.$inject = ['$routeProvider'];
-function candidateConfig($routeProvider) {
-    $routeProvider.when('/candidate/:name', {
-        templateUrl: 'components/candidate/candidate.html',
-        controller: 'CandidateCtrl',
-        resolve: {
-            candidate: ['candidatesService', '$route', function(candidatesService, $route) {
-                return candidatesService.getCandidateDetails($route.current.params.name);
-            }],
-            name: ['$route', function($route) {
-                return $route.current.params.name;
-            }],
-        }
-    });
-}
-
-CandidateCtrl.$inject = ['$scope', 'candidate', 'name'];
-function CandidateCtrl($scope, candidate, name) {
-    $scope.name = name;
-    $scope.candidate = candidate;
-    $scope.contributors = candidate.contributors;
-    $scope.industries = candidate.industries;
-    $scope.typeBreakdown = candidate.types;
-
-    $scope.options = {
-        responsive: true
-    };
-
-    var lastName = name.split(',')[0].replace("'","");
-
-    $scope.headshotsrc = "./assets/images/" + lastName + ".jpg";
+(function() {
+    'use strict';
 
 
-    var industryNamesArray = [];
-    var industryDataArray = [];
+    angular.module('cfa.components.candidate', ['ngRoute','chart.js','cfa.directives.cfaImage'])
+        .config(candidateConfig)
+        .controller('CandidateCtrl', CandidateCtrl);
 
-    if ($scope.industries) {
-        $scope.industries.forEach(function(c) {
-            industryNamesArray.push(c.name);
-            industryDataArray.push(c.amount);
+    candidateConfig.$inject = ['$routeProvider'];
+    function candidateConfig($routeProvider) {
+        $routeProvider.when('/candidate/:name', {
+            templateUrl: 'components/candidate/candidate.html',
+            controller: 'CandidateCtrl',
+            resolve: {
+                candidate: ['candidatesService', '$route', function(candidatesService, $route) {
+                    return candidatesService.getCandidateDetails($route.current.params.name);
+                }],
+                name: ['$route', function($route) {
+                    return $route.current.params.name;
+                }],
+            }
         });
-    };
+    }
 
-    $scope.labels = industryNamesArray;
-    $scope.data = industryDataArray;
+    CandidateCtrl.$inject = ['$scope', 'candidate', 'name'];
+    function CandidateCtrl($scope, candidate, name) {
+        $scope.name = name;
+        $scope.candidate = candidate;
+        $scope.contributors = candidate.contributors;
+        $scope.industries = candidate.industries;
+        $scope.typeBreakdown = candidate.types;
 
-}
+        $scope.options = {
+            responsive: true
+        };
+
+        var lastName = name.split(',')[0].replace("'","");
+
+        $scope.headshotsrc = "./assets/images/" + lastName + ".jpg";
+
+
+        var industryNamesArray = [];
+        var industryDataArray = [];
+
+        if ($scope.industries) {
+            $scope.industries.forEach(function(c) {
+                industryNamesArray.push(c.name);
+                industryDataArray.push(c.amount);
+            });
+        };
+
+        $scope.labels = industryNamesArray;
+        $scope.data = industryDataArray;
+
+    }
+
+})();
